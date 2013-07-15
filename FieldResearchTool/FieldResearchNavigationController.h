d98f948debfc2d9eef7ea032224770725ccb26bd
m1sterc001guy <jgmoeller@wisc.edu>
2013-07-15 12:26:02 -0500

    Basic observation controller

---
+++ FieldResearchTool/FieldResearchNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  FieldResearchNavigationController.h
+//  FieldResearchTool
+//
+//  Created by Justin Moeller on 7/15/13.
+//  Copyright (c) 2013 UW Mobile Learning Incubator. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FieldResearchNavigationController : UINavigationController
+
+@end
