31f5b0323f52a4f29f5304e73e493eceba3315f4
m1sterc001guy <jgmoeller@wisc.edu>
2013-07-25 15:32:16 -0500

    Added enumJudgementViewController and created switch statement for setting up views

--- FieldResearchTool/ObservationContainerViewController.m
+++ FieldResearchTool/ObservationContainerViewController.m
@@ -9,6 +9,9 @@
 #import "ObservationContainerViewController.h"
 #import "AppModel.h"
 #import "ProjectComponentPossibility.h"
+#import "ObservationDataType.h"
+#import "ObservationJudgementType.h"
+#import "EnumJudgementViewController.h"
 
 
 @interface ObservationContainerViewController (){
@@ -56,7 +59,63 @@
     
     [self.navigationItem setRightBarButtonItem:[[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemSave target:self action:@selector(saveObservationData:)]];
     
-    NSLog(@"IN CONTAINER");
+    switch ([projectComponent.observationDataType intValue]) {
+        case DATA_AUDIO:
+            //set up view controller here
+            break;
+        case DATA_VIDEO:
+            //set up view controller here
+            break;
+        case DATA_PHOTO:
+            //set up view controller here
+            break;
+        case DATA_NUMBER:
+            //set up view controller here
+            break;
+        case DATA_BOOLEAN:
+            //set up view controller here
+            break;
+        case DATA_TEXT:
+            //set up view controller here
+            break;
+        case DATA_LONG_TEXT:
+            //set up view controller here
+            break;
+        case DATA_ENUMERATOR:
+            //set up view controller here
+            break;
+        default:
+            break;
+    }
+    
+    UIViewController *viewControllerToDisplay;
+    switch ([projectComponent.observationJudgementType intValue]) {
+        case JUDGEMENT_NUMBER:
+            //set up view controller here
+            break;
+        case JUDGEMENT_BOOLEAN:
+            //set up view controller here
+            break;
+        case JUDGEMENT_TEXT:
+            //set up view controller here
+            break;
+        case JUDGEMENT_LONG_TEXT:
+            //set up view controller here
+            break;
+        case JUDGEMENT_ENUMERATOR:{
+            //EnumJudgementViewController *enumJudgeViewController = [[EnumJudgementViewController alloc]initWithNibName:@"EnumJudgementViewController" bundle:nil];
+            EnumJudgementViewController *enumJudgeViewController = [[EnumJudgementViewController alloc]init];
+            enumJudgeViewController.view.frame = CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, ([UIScreen mainScreen].bounds.size.height + 68));
+            NSLog(@"X: %f Y: %f Width: %f Height: %f", enumJudgeViewController.view.frame.origin.x, enumJudgeViewController.view.frame.origin.y, enumJudgeViewController.view.frame.size.width, enumJudgeViewController.view.frame.size.height);
+            viewControllerToDisplay = enumJudgeViewController;
+        }
+            break;
+        default:
+            break;
+    }
+    
+    [self addChildViewController:viewControllerToDisplay];
+    [self.view addSubview:viewControllerToDisplay.view];
     
 }
 
