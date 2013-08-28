247f4749449e671c0e599140bd230b0eba018bd9
m1sterc001guy <jgmoeller@wisc.edu>
2013-08-28 14:18:03 -0500

    Added border around choosing enum

--- FieldResearchTool/EnumJudgementViewController.m
+++ FieldResearchTool/EnumJudgementViewController.m
@@ -13,6 +13,7 @@
 #import "SaveObservationAndJudgementDelegate.h"
 #import "MediaManager.h"
 #import "UserObservationComponentData.h"
+#import <QuartzCore/QuartzCore.h>
 
 @interface EnumJudgementViewController () <iCarouselDataSource, iCarouselDelegate, UIActionSheetDelegate, SaveJudgementDelegate>{
     ProjectComponentPossibility *chosenPossibility;
@@ -161,6 +162,8 @@
         //get a reference to the label in the recycled view
         label = (UILabel *)[view viewWithTag:1];
         label.textColor = [UIColor blackColor];
+        UIImageView *imageView = (UIImageView *)view;
+        [imageView.layer setBorderWidth:0.0f];
     }
     
     //set item label
@@ -195,6 +198,9 @@
     if(chosenPossibility){
         if([componentPossibility.enumValue isEqualToString:chosenPossibility.enumValue]){
             label.textColor = [UIColor whiteColor];
+            UIImageView *imageView = (UIImageView *)view;
+            [imageView.layer setBorderColor: [[UIColor blackColor] CGColor]];
+            [imageView.layer setBorderWidth: 2.0];
         }
     }
     
